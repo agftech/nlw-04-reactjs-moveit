67e71a05c983c0b34148a6e8f450df3a3882a7f3
agftech <agiraof@gmail.com>
2021-02-22 16:08:41 -0800

    feat: :package: add ReactJS project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <ExperienceBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
