adbf0fa505b1a13b8daf2428588bb9060df3f413
agftech <agiraof@gmail.com>
2021-02-24 15:20:02 -0800

    feat: :package: add experiencebar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+
 import Head from 'next/head';
 
-import styles from '../styles/pages/Home.module.css';
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home | move.it</title>
-      </Head>
-      Hello NLW#4.0 - Move.it App with next.js
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Home | move.it</title>
+            </Head>
+
+            <ExperienceBar />
+            
+        </div>
+    )
 }
