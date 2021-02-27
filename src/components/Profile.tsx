2d066e8500f63fdbb82e3814d080c22a5b6adacc
agftech <agiraof@gmail.com>
2021-02-27 10:06:01 -0800

    feat: :package: add context to profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import React, { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/agftech.png" alt="agftech" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>AGFTECH`s</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
